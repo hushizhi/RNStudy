df41f60f57cf63c179a63487a490b18a78600fa5
戚灼伟 <qizhuowei@meituan.com>
2018-09-19 09:37:22 +0800

    练习2

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,26 +1,19 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from 'react';
+import { AppRegistry, View} from 'react-native';
 
-class Greeting extends Component {
+export default class FixedDimensionsBasics extends Component {
   render() {
     return (
-      <Text> Hello {this.props.name} ! </Text>
-    );
-  }
-}
-
-export default class LotsOfGreeting extends Component {
-  render() {
-    return (
-      <View style={{alignItems: 'center'}}>
-        <Greeting name='你好吗' />
-        <Greeting name='你是谁' />
-        <Greeting name='你在哪里' />
-        <Greeting name='你好吗' />
-        <Greeting name='你是谁' />
-        <Greeting name='你在哪里' />
+      <View>
+        <View style={{width: 50, height: 50, backgroundColor: 'red'}} />
+        <View style={{width: 100, height: 100, backgroundColor: 'yellow'}} />
+        <View style={{width: 150, height: 150, backgroundColor: 'blue'}} />
       </View>
     );
   }
+};
+// 注册应用(registerComponent)后才能正确渲染
+// 注意：只把应用作为一个整体注册一次，而不是每个组件/模块都注册
+AppRegistry.registerComponent('AwesomeProject', () => FixedDimensionsBasics);
 
-}
+// AppRegistry.registerComponent('FixedDimensionsBasics', () => FixedDimensionsBasics);
