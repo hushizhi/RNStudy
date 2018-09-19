2e420c1e312bf3cdcd05b717e0321a25e8566100
戚灼伟 <qizhuowei@meituan.com>
2018-09-19 09:05:23 +0800

    练习1

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,43 +1,26 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+class Greeting extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>你好，RN。</Text>
-        <Text style={styles.instructions}>山竹台风，RN。</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Text> Hello {this.props.name} ! </Text>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 25,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default class LotsOfGreeting extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='你好吗' />
+        <Greeting name='你是谁' />
+        <Greeting name='你在哪里' />
+        <Greeting name='你好吗' />
+        <Greeting name='你是谁' />
+        <Greeting name='你在哪里' />
+      </View>
+    );
+  }
+
+}
