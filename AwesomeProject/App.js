16b597f09b0ac0d7b4344ad501e26cc1d81936e6
戚灼伟 <qizhuowei@meituan.com>
2018-09-22 16:05:53 +0800

    scrollView

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
-import { AppRegistry, View, Text, TextInput } from 'react-native';
+import { ScrollView, Image, Text, View } from 'react-native';
 
-export default class PizzaTranslator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {text: ''};
-  }
-
+export default class IScrolledDownAndWhatHappenedNextShockedMe extends Component {
   render() {
     return (
-      <View style={{padding: 10}}>
-        <TextInput
-          style={{height: 40}}
-          placeholder = "你好嘛"
-          onChangeText={(text) => this.setState({text})}
-        />
-        <Text style={{padding: 10, fontSize: 42}}>
-          {this.state.text.split(' ').map((word) => word && '📷').join(' ')}
-        </Text>
-      </View>
+      <ScrollView 
+        horizontal={true}
+        pagingEnabled={true}
+        showsHorizontalScrollIndicator={true}
+        >
+        <Text style={{fontSize: 35}}> hello world! </Text>
+        <Image source={require('./q.png')} />
+        <Text style={{fontSize: 30}}> ScrollView mp plz </Text>
+        <Image source={require('./q.png')} />
+        <Text style={{fontSize: 30}}> ScrollView mp zhong </Text>
+        <Image source={require('./q.png')} />
+        <Image source={require('./q.png')} />
+        <Text style={{fontSize: 30}}> ScrollView mp xia </Text>
+        <Image source={require('./q.png')} />
+        <Image source={require('./q.png')} />
+      </ScrollView>
     );
   }
-};
-
-// 注册应用(registerComponent)后才能正确渲染
-// 注意：只把应用作为一个整体注册一次，而不是每个组件/模块都注册
-AppRegistry.registerComponent('AwesomeProject', () => PizzaTranslator);
+}
